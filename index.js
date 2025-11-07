5bed6015473f1be19555de9d9f12a2efe2bb5b67
GabriLazovsky <gabriel.lazovskyigual@usp.ceu.es>
2025-11-07 13:32:18 +0100

    ejhecho

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Happy developing ✨')
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const port = 3000;
+
+
+const palabras = fs.readFileSync('diccionario.txt', 'utf-8')
+    .split(/\r?\n/)
+    .filter(Boolean);
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+    const queryObject = url.parse(req.url, true).query;
+    const numPalabras = parseInt(queryObject.x) || 3; // valor por defecto = 3
+
+
+    let seleccionadas = [];
+    for (let i = 0; i < numPalabras; i++) {
+        const rand = Math.floor(Math.random() * palabras.length);
+        seleccionadas.push(palabras[rand]);
+    }
+    const password = seleccionadas.join('-');
+
+    res.write(`<h1>Contraseña generada (${numPalabras} palabras):</h1>`);
+    res.write(`<p><b>${password}</b></p>`);
+    res.write(`<p>Ejemplo: <a href="/?x=5">Generar con 5 palabras</a></p>`);
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
+});
